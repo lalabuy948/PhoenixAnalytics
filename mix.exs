ba45a1d2b8aa604e740be2ff3057aada69d814cc
Stu Page <stuartjohnpage@live.com>
2025-09-05 10:39:19 -0500

    use built-in Ecto UUIDs

--- mix.exs
+++ mix.exs
@@ -65,7 +65,6 @@ defmodule PhoenixAnalytics.MixProject do
       {:ecto_sql, "~> 3.10"},
       {:telemetry, "~> 1.2"},
       {:live_react, "~> 1.1"},
-      {:elixir_uuid, "~> 1.2"},
       {:phoenix_live_view, "~> 1.1"},
       # --- dev deps ---
       {:ex_doc, "~> 0.33", only: :dev},
